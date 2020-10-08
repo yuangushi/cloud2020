da96924c9f4d1d8fec32e11c1c1b46ee76af4fcb
Administrator <1220979243@qq.com>
2020-10-08 21:27:23 +0800

    创建dao.service.controller

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/web/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.web;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping(value="/payment/create")
+    @ResponseBody
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("***插入结果："+result);
+        if(result>0){
+            return new CommonResult(200,"插入数据库成功",result);
+        }else{
+            return new CommonResult(444,"插入失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    @ResponseBody
+    public CommonResult getPaymentById(@PathVariable(name = "id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("***查询结果："+payment);
+        if(payment!=null){
+            return new CommonResult(200,"插入数据库成功",payment);
+        }else{
+            return new CommonResult(444,"没有对应记录，查询ID是:"+id,null);
+        }
+    }
+
+}
